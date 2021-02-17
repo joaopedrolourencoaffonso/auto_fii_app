1cd0a1cd350194d20d518f9a8c317eb45b2d14a3
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2021-02-17 10:49:32 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+#Joao Pedro L. Affonso
+#####    
+
+def main():
+    import math;
+
+    opcao = input("O que você quer? \n -> Digite 1 para cos^(-1) \n -> Digite 2 para sen^(-1) \nDigite aqui:");
+
+    if opcao == "1":
+        x = float(input("Digite o angulo: "));
+        y = 180*(math.acos(x)/math.pi);
+        print("O cos^(-1) de " + str(x) + " é: " + str(y));
+
+    elif opcao == "2":
+        x = float(input("Digite o angulo: "));
+        y = 180*(math.asin(x)/math.pi);
+        print("O cos^(-1) de " + str(x) + " é: " + str(y));
+
+if __name__ == '__main__':
+    main()
